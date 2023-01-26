7f0c535ef575dc49b22b6374bfe9627445db45a5
Reed W Priest <pries153@umn.edu>
2023-01-26 11:15:05 -0600

    Lines 3:5 done.

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,4 @@
 # Importing and Labeling
 rt_df <- read.csv("data/week2.csv", header = TRUE)
+rt_df$condition[rt_df$condition == "A"] <- "Control" ; rt_df$condition[rt_df$condition == "B"] <- "Experimental" 
+rt_df$gender[rt_df$gender == "M"] <- "Male" ; rt_df$gender[rt_df$gender == "F"] <- "Female" ; rt_df$gender[rt_df$gender == "N"] <- "Nonbinary"
