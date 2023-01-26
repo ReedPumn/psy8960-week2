69cce3cf87d9086aa37d7127114fbf379e5086c1
Reed W Priest <pries153@umn.edu>
2023-01-26 11:16:07 -0600

    Lines 6:9 done.

--- R/week2.R
+++ R/week2.R
@@ -2,3 +2,8 @@
 rt_df <- read.csv("data/week2.csv", header = TRUE)
 rt_df$condition[rt_df$condition == "A"] <- "Control" ; rt_df$condition[rt_df$condition == "B"] <- "Experimental" 
 rt_df$gender[rt_df$gender == "M"] <- "Male" ; rt_df$gender[rt_df$gender == "F"] <- "Female" ; rt_df$gender[rt_df$gender == "N"] <- "Nonbinary"
+
+# Analysis
+mean(rt_df$rt)
+rt_f_df <- subset(rt_df, rt_df$gender == "Female")
+hist(rt_f_df$rt)
